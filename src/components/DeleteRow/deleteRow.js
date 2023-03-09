d87e44a96a0704922e406c623c72c0b048ff545f
trần kim đạt <kimdat1999@gmail.com>
2023-03-09 09:26:34 +0700

    fitotalrowdelete

--- src/components/DeleteRow/deleteRow.js
+++ src/components/DeleteRow/deleteRow.js
@@ -6,101 +6,74 @@ import { api } from "../../Interceptor";
 
 const API_URL = api.defaults.baseURL;
 
-const DeleteRow = React.memo(
-  ({
-    rowsId,
-    data,
-    setSearchApiData,
-    setCheckedRows,
-    loadData,
-    setIsLoading,
-  }) => {
-    const names = useMemo(() => {
-      let STT = 0;
-      return rowsId
-        .filter((item) => typeof item.name != "undefined")
-        .map((item) => ++STT + "." + item.name);
-    }, [rowsId]);
+const DeleteRow = React.memo(({ rowsId, loadData }) => {
+  const names = useMemo(() => {
+    let STT = 0;
+    return rowsId
+      .filter((item) => typeof item.name != "undefined")
+      .map((item) => ++STT + "." + item.name);
+  }, [rowsId]);
 
-    const confirmDelete = useCallback(async () => {
-      console.log(rowsId);
-      if (rowsId.length === 0 || typeof rowsId[0] == "undefined") {
+  const confirmDelete = useCallback(async () => {
+    console.log(rowsId);
+    if (rowsId.length === 0 || typeof rowsId[0] == "undefined") {
+      Swal.fire({
+        icon: "error",
+        text: "Please choose device to delete",
+      });
+      return;
+    }
+    const result = await Swal.fire({
+      title: "confirmDelete!Are you sure delete devices!",
+      icon: "error",
+      html: `<div style="max-height:300px;overflow-y:auto"><div >${names.join(
+        "<br/>"
+      )}</div></div>`,
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+      cancelButtonText: "No",
+    });
+    return result.isConfirmed;
+  }, [names, rowsId]);
+
+  const handleDelete = useCallback(async () => {
+    if (await confirmDelete()) {
+      const arrId = rowsId.map((item) => item.id);
+      const params = {
+        rowsId: JSON.stringify(arrId),
+      };
+      try {
+        // setIsLoading(true);
+        await api.get(`${API_URL}deleteRow`, { params });
+        loadData();
+        Swal.fire({
+          icon: "success",
+          text: "Delete success",
+        });
+      } catch (err) {
+        console.error(err);
+        const message = err?.response?.data?.error ?? err?.error ?? err;
         Swal.fire({
           icon: "error",
-          text: "Please choose device to delete",
+          text: `Error  handDelete() in deleteRow.js ${message}`,
         });
-        return;
       }
-      const result = await Swal.fire({
-        title: "confirmDelete!Are you sure delete devices!",
-        icon: "error",
-        html: `<div style="max-height:300px;overflow-y:auto"><div >${names.join(
-          "<br/>"
-        )}</div></div>`,
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes",
-        cancelButtonText: "No",
-      });
-      return result.isConfirmed;
-    }, [names, rowsId]);
+    }
+  }, [confirmDelete, loadData, rowsId]);
 
-    const handleDelete = useCallback(async () => {
-      if (await confirmDelete()) {
-        const arrId = rowsId.map((item) => item.id);
-        const params = {
-          rowsId: JSON.stringify(arrId),
-        };
-        try {
-          setIsLoading(true);
-          await api.get(`${API_URL}deleteRow`, { params });
-          const newData = data.filter(
-            (row) => !arrId.includes(row.id) && !arrId.includes(row.ParentId)
-          );
-          if (newData.length === 0) {
-            loadData();
-          } else {
-            setSearchApiData(newData);
-          }
-          setIsLoading(false);
-          setCheckedRows([]);
-          Swal.fire({
-            icon: "success",
-            text: "Delete success",
-          });
-        } catch (err) {
-          setIsLoading(false);
-          console.error(err);
-          const message = err?.response?.data?.error ?? err?.error ?? err;
-          Swal.fire({
-            icon: "error",
-            text: `Error  handDelete() in deleteRow.js ${message}`,
-          });
-        }
-      }
-    }, [
-      rowsId,
-      data,
-      setSearchApiData,
-      setCheckedRows,
-      confirmDelete,
-      loadData,
-      setIsLoading,
-    ]);
-
-    return (
-      <div>
-        <Button
-          variant="danger"
-          onClick={handleDelete}
-          style={{ marginLeft: "10px", borderRadius: "10px" }}
-        >
-          <FaTrash />
-        </Button>
-      </div>
-    );
-  }
-);
+  return (
+    <div>
+      <Button
+        variant="danger"
+        onClick={handleDelete}
+        style={{ marginLeft: "10px", borderRadius: "10px" }}
+      >
+        <FaTrash />
+      </Button>
+    </div>
+  );
+});
 
 export default DeleteRow;
