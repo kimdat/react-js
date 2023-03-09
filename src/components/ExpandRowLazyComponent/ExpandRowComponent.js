980bd689a4851c3bcd75455335d6552a87d24c56
trần kim đạt <kimdat1999@gmail.com>
2023-03-09 17:43:10 +0700

    fixwidthcolumn

--- src/components/ExpandRowLazyComponent/ExpandRowComponent.js
+++ src/components/ExpandRowLazyComponent/ExpandRowComponent.js
@@ -2,30 +2,32 @@ import React, { useMemo } from "react";
 import DataTable from "react-data-table-component";
 
 const ExpandRowComponent = React.memo(({ row }) => {
-
   const columnsChild = useMemo(
     () => [
       {
         name: "",
         selector: "",
-        width: "130px",
+        width: "9.5%",
       },
       {
         name: "Name",
         selector: (row) => row["Name"],
-        width: "280px",
+        width: "20%",
       },
       {
         name: "VID",
         selector: (row) => row["VID"],
+        width: "15%",
       },
       {
         name: "Serial",
         selector: (row) => row["Serial"],
+        width: "14.5%",
       },
       {
         name: "PID",
         selector: (row) => row["PID"],
+        width: "15%",
       },
       {
         name: "CDESC",
