dd7b7d99d06bb3a13e13611934b5f95fd8c566f2
trần kim đạt <kimdat1999@gmail.com>
2023-03-11 07:03:16 +0700

    addreactlazyforrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login } from "./pages/Login/Login";
-import LogoutPage from "./pages/Logout";
-import Inventories from "./pages/Inventories/Inventories";
+
 import Layout from "./LAYOUT/Layout.js";
-import Inventories1 from "./pages/Inventories1";
-import InventoriesOnline from "./pages/InventoriesOnline/InventoriesOnline";
+const Login = React.lazy(() => import("./pages/Login/Login"));
+const Inventories = React.lazy(() =>
+  import("./pages/DeviceInventoryManage/DeviceInventoryManage")
+);
+const InventoriesOnline = React.lazy(() =>
+  import("./pages/InventoriesOnline/InventoriesOnline")
+);
 
 function App() {
   const isLoggedIn = !!localStorage.getItem("email");
@@ -20,11 +23,14 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
-          <Route exact path="/logout" element={<LogoutPage />} />
           <Route
             exact
             path="/inventories"
-            element={<Layout><Inventories /></Layout>}
+            element={
+              <Layout>
+                <Inventories flagOffline={true} />
+              </Layout>
+            }
           ></Route>
           <Route
             exact
@@ -37,7 +43,17 @@ function App() {
               </Layout>
             }
           ></Route>
-          <Route exact path="/inventories1" element={<Inventories1 />}></Route>
+          <Route
+            exact
+            path="/managementDeviceInventories"
+            element={
+              <Layout>
+                <div>
+                  <Inventories />
+                </div>
+              </Layout>
+            }
+          ></Route>
         </Routes>
       </Router>
     </div>
