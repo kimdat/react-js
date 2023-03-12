55eebe1a56ae6f5b97ace5395c9e98e7eb777458
trần kim đạt <kimdat1999@gmail.com>
2023-03-12 12:30:36 +0700

    split useeffect into component

--- src/pages/InventoriesOnline/InventoriesComponentOnline.js
+++ src/pages/InventoriesOnline/InventoriesComponentOnline.js
@@ -4,20 +4,14 @@ import InventoriesComponent from "../../components/InventoriesComponent/Inventor
 
 import LoadingComponent from "../../components/LoadingComponent/LoadingComponent";
 import "./InventoriesOnline.css";
-
 const InventoriesComponentOnline = React.memo(({ apiData }) => {
+  console.log("invComponentOnline");
+  //những row được expand
   const [rowExpand, setRowExpand] = useState([]);
   //datapage hiện tại
   const [searchApiData, setSearchApiData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  //số row mỗi page
-  const [rowsPerPage, setRowsPerPage] = useState(10);
   //tổng row
   const [totalRow, setToTalRow] = useState(0);
-  //Mở rộng hết
-  const [isExpandedAll, setIsExpandedAll] = useState(false);
-  //Hàm loading
-  const [isLoading, setIsLoading] = useState(false);
   //tổng data
   const [dataAll, setDataAll] = useState([]);
   useEffect(() => {
@@ -26,57 +20,93 @@ const InventoriesComponentOnline = React.memo(({ apiData }) => {
     setToTalRow(apiData.total_row);
     setDataAll(apiData.data);
   }, [apiData]);
-  const handleDatatable = useCallback(
-    (currentPage, rowsPerPage) => {
-      const start = (currentPage - 1) * rowsPerPage;
-      const end = start + rowsPerPage;
-      setSearchApiData(dataAll.slice(start, end));
-    },
-    [dataAll]
-  );
-  console.log("iv");
-  const handlePageChange = useCallback(
-    (page) => {
-      setCurrentPage(page);
-      handleDatatable(page, rowsPerPage);
-    },
-    [handleDatatable, rowsPerPage]
-  );
-  const handleRowsPerPageChange = useCallback(
-    (data) => {
-      setRowsPerPage(data);
-      handleDatatable(currentPage, data);
-    },
-    [handleDatatable, currentPage]
-  );
 
   return (
     <div>
       {searchApiData.length > 0 && (
-        <LoadingComponent isLoading={isLoading}>
-          <MDBContainer fluid>
-            <MDBRow className="d-flex justify-content-center align-items-center h-100">
-              <MDBCardBody>
-                <InventoriesComponent
-                  totalRow={totalRow}
-                  currentPage={currentPage}
-                  handlePageChange={handlePageChange}
-                  rowsPerPage={rowsPerPage}
-                  handleRowsPerPageChange={handleRowsPerPageChange}
-                  setIsLoading={setIsLoading}
-                  searchApiData={searchApiData}
-                  setSearchApiData={setSearchApiData}
-                  isExpandedAll={isExpandedAll}
-                  setIsExpandedAll={setIsExpandedAll}
-                  rowExpand={rowExpand}
-                  setRowExpand={setRowExpand}
-                />
-              </MDBCardBody>
-            </MDBRow>
-          </MDBContainer>
-        </LoadingComponent>
+        <InventoriesComponentOnlineChild
+          searchApiData={searchApiData}
+          setSearchApiData={setSearchApiData}
+          totalRow={totalRow}
+          rowExpand={rowExpand}
+          setRowExpand={setRowExpand}
+          dataAll={dataAll}
+        />
       )}
     </div>
   );
 });
+
+const InventoriesComponentOnlineChild = React.memo(
+  ({
+    searchApiData,
+    setSearchApiData,
+    totalRow,
+    rowExpand,
+    setRowExpand,
+    dataAll,
+  }) => {
+    console.log("invcomponentchild");
+    const [currentPage, setCurrentPage] = useState(1);
+    //số row mỗi page
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    //Mở rộng hết
+    const [isExpandedAll, setIsExpandedAll] = useState(false);
+    //Hàm loading
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleDatatable = useCallback(
+      (currentPage, rowsPerPage) => {
+        const start = (currentPage - 1) * rowsPerPage;
+        const end = start + rowsPerPage;
+        setSearchApiData(dataAll.slice(start, end));
+      },
+      [dataAll, setSearchApiData]
+    );
+
+    const handlePageChange = useCallback(
+      (page) => {
+        setCurrentPage(page);
+        handleDatatable(page, rowsPerPage);
+      },
+      [handleDatatable, rowsPerPage]
+    );
+    const handleRowsPerPageChange = useCallback(
+      (data) => {
+        setRowsPerPage(data);
+        handleDatatable(currentPage, data);
+      },
+      [handleDatatable, currentPage]
+    );
+
+    return (
+      <div>
+        {searchApiData.length > 0 && (
+          <LoadingComponent isLoading={isLoading}>
+            <MDBContainer fluid>
+              <MDBRow className="d-flex justify-content-center align-items-center h-100">
+                <MDBCardBody>
+                  <InventoriesComponent
+                    totalRow={totalRow}
+                    currentPage={currentPage}
+                    handlePageChange={handlePageChange}
+                    rowsPerPage={rowsPerPage}
+                    handleRowsPerPageChange={handleRowsPerPageChange}
+                    setIsLoading={setIsLoading}
+                    searchApiData={searchApiData}
+                    setSearchApiData={setSearchApiData}
+                    isExpandedAll={isExpandedAll}
+                    setIsExpandedAll={setIsExpandedAll}
+                    rowExpand={rowExpand}
+                    setRowExpand={setRowExpand}
+                  />
+                </MDBCardBody>
+              </MDBRow>
+            </MDBContainer>
+          </LoadingComponent>
+        )}
+      </div>
+    );
+  }
+);
 export default InventoriesComponentOnline;
