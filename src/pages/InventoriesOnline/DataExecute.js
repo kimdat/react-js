b6a5579e43600ef37dea349aa0b1ca10b0c86529
trần kim đạt <kimdat1999@gmail.com>
2023-03-15 13:42:46 +0700

    fix

--- src/pages/InventoriesOnline/DataExecute.js
+++ src/pages/InventoriesOnline/DataExecute.js
@@ -6,12 +6,10 @@ import React, {
   useImperativeHandle,
 } from "react";
 import InventoriesComponentOnline from "./InventoriesComponentOnline";
-import { api } from "../../Interceptor";
+
 import Swale from "sweetalert2";
 import LoadingComponent from "../../components/LoadingComponent/LoadingComponent";
 import axios from "axios";
-
-const API_URL = api.defaults.baseURL;
 const DataExecute = memo(
   forwardRef((props, ref) => {
     const [rowExpand, setRowExpand] = useState([]);
@@ -22,50 +20,44 @@ const DataExecute = memo(
     //tổng data
     const [dataAll, setDataAll] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const getExecute = async (device_list, jsonId) => {
+    const getExecute = async (device_list) => {
       try {
         setIsLoading(true);
         const formData = new FormData();
-        //formData.append("device_list", JSON.stringify(device_list));
-        //formData.append("jsonId", JSON.stringify(jsonId));
+
         formData.append("ip", JSON.stringify(device_list));
         const { data } = await axios.post(
           "http://localhost/NETMIKO/home.py",
           formData
         );
-        var inventory = data.deviceData;
-        var devicesName = data.deviceName;
 
-        inventory = JSON.parse(inventory);
-
-        var dataSuccess = [];
-        var dataFail = [];
-        var stt = 1;
-        for (var ip in devicesName) {
-          var deviceName = devicesName[ip];
-          var dataInventory = inventory[ip];
-
-          var dataInventoryFirst = dataInventory[0];
+        const inventory = JSON.parse(data.deviceData);
+        const dataSuccess = [];
+        const dataFail = [];
+        let stt = 1;
+        device_list.forEach((device) => {
+          const ip = device.ip;
+          const dataInventory = inventory[ip];
           var children = [];
-          if (!dataInventoryFirst.Err) {
+          //nếu connect success
+          if (!dataInventory[0].Err) {
             children = dataInventory;
             dataSuccess.push({
               STT: stt++,
               ip: ip,
-              id: jsonId[ip],
-              Name: deviceName,
+              id: device.id,
+              Name: device.deviceName,
               children: children,
             });
           } else {
             dataFail.push({
               ip: ip,
-              id: jsonId[ip],
-              Name: deviceName,
+              id: device.id,
+              Name: device.deviceName,
               children: children,
             });
           }
-        }
-
+        });
         if (dataFail.length > 0) {
           const messFail = dataFail.map((i) => i.ip);
           Swale.fire({
@@ -91,8 +83,9 @@ const DataExecute = memo(
         console.error(message);
       }
     };
-    const handleClick = useCallback(async (rowCheck, jsonId) => {
-      getExecute(rowCheck, jsonId);
+    const handleClick = useCallback(async (rowCheck) => {
+      console.log(rowCheck);
+      getExecute(rowCheck);
     }, []);
     useImperativeHandle(ref, () => ({
       // Định nghĩa hàm handleClick() để có thể gọi từ cha
