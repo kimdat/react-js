8fecdced25a469a935968ad560f95b7b04e124c1
Nguyễn Bá Đức <nguyenbaduc355@gmail.com>
2023-03-20 14:02:07 +0700

    A minor fix

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080",
+  baseUrl: "http://localhost/APILOGIN",
   prepareHeaders: (headers, { getState }) => {
     const token = process.env.REACT_APP_TOKEN_KEY;
     if (token) {
